f3490cf0bb48e90ac2c7f5a0c871dae7a14fb995
imane <imaneeddouni2000@gmail.com>
2025-05-30 17:06:57 +0100

    add userController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    public function register(Request $request)
+    {
+        // return response()->json(true);
+        // $request->validate([
+        
+        // ])
+    }
+
+    public function login() {}
+
+    public function logout() {}
+}
