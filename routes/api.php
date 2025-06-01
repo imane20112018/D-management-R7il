db1f27315f0dbe68ee5e20e2294bbe2b9f65877f
Imane Eddouni <66179325+imane20112018@users.noreply.github.com>
2025-06-02 00:33:27 +0100

    add Login and logout routes

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,13 @@
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Types\Relations\Role;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 
-Route::get('register', [UserController::class, 'register']);
+Route::post('register', [UserController::class, 'register']);
+Route::post('login',[UserController::class,'login']);
+Route::post('logout',[UserController::class,'logout']);
