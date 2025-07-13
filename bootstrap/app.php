98742464ff0d5112bc53127051b04fa43f6ade8b
Imane Eddouni <66179325+imane20112018@users.noreply.github.com>
2025-07-14 00:18:34 +0100

    register middlware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\IsAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -17,3 +18,6 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+    $app->routeMiddleware([
+    'admin' => IsAdmin::class,
+    ]);
