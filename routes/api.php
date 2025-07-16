d805d5685a19e507ea1fa22446c758644495c1ef
imane <imaneeddouni2000@gmail.com>
2025-07-16 19:04:25 +0100

    add right route for clients list

--- routes/api.php
+++ routes/api.php
@@ -23,7 +23,8 @@ Route::middleware('auth:sanctum')->post('/logout', [UserController::class, 'logo
 Route::middleware(['auth:sanctum', 'admin'])->get('/admin/dashboard', function () {
     return response()->json(['message' => 'Welcome, Admin']);
 });
-Route::middleware(['auth:sanctum', 'admin'])->get('/admin/clients', [AuthController::class, 'getClients']);
+//Route::middleware(['auth:sanctum', 'admin'])->get('/admin/clients', [AuthController::class, 'getClients']);
+Route::get('/clients', [AuthController::class, 'getClients']);
 
 
 /*
